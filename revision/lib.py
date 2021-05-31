abe3e076de4852b14c823ccec9eaa7eec992abf4
Marie Chistyakova <mallliya@hotmail.com>
2021-05-31 16:42:09 +0200

    update on tools

--- revision/lib.py
+++ revision/lib.py
@@ -1,3 +1,6 @@
+import requests
+from requests.models import Response
+
 def handle_duplicates(dataset):
     dataset=dataset.drop_duplicates()
     return dataset
@@ -9,3 +12,8 @@ def handle_na(dataset):
             dataset.drop(columns=i,inplace=True)
     return dataset
 
+def get_weather_forecast(city_name):
+    b_url = f'api.openweathermap.org/data/2.5/weather?q={city_name}&appid={6d1685749aafd24c47b05d19d427673f}'
+    response = requests.get(b_url)
+    temp = round((response['main']['temp']-32)/1.8,1)
+    return temp, response['weather'][0]['main']
---
+++ tests/test_handle_na.py
@@ -0,0 +1,5 @@
+def test_handle_duplicates():
+    pass #assert len(handle_duplicates())!=0
+
+def test_handle_na():
+    pass #assert len(handle_na())!=0
